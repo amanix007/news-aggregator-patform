c9d7c0984ce64a6c57a1f2e9d47a5ddede960b12
Aman Ullah <amanix007@gmail.com>
2024-03-25 05:42:38 +0600

    compiling differenct data sources

--- src/misc/common.tsx
+++ src/misc/common.tsx
@@ -1,41 +1,51 @@
 import { NewsSources } from "../config/constants";
 import api from "../services";
 
-export const searchInNewsapi =async (queryString: String)=> {
+export const searchInNewsapi = async (queryString: string) => {
   try {
-    const { data } = await api.get(`${NewsSources.newsapi.baseURL}/everything?q=${queryString}&apiKey=${NewsSources.newsapi.apiKey}&page=1&pageSize=10`);
+    const { data } = await api.get(
+      `${NewsSources.newsapi.baseURL}/everything?q=${queryString}&apiKey=${NewsSources.newsapi.apiKey}&page=1&pageSize=10`
+    );
     return data;
   } catch (error) {
     return api.handleError(error);
   }
-}
-export const searchIntheGurdian =async (queryString: String)=> {
+};
+export const searchIntheGurdian = async (queryString: String) => {
   try {
-    const { data } = await api.get(`${NewsSources.theguardian.baseURL}/search?q=${queryString}&api-key=${NewsSources.theguardian.apiKey}`);
+    const { data } = await api.get(
+      `${NewsSources.theguardian.baseURL}/search?q=${queryString}&api-key=${NewsSources.theguardian.apiKey}`
+    );
     return data;
   } catch (error) {
     return api.handleError(error);
   }
-}
-export const searchInNyTimes =async (queryString: String)=> {
+};
+export const searchInNyTimes = async (queryString: String) => {
   try {
-    const { data } = await api.get(`${NewsSources.nytimes.baseURL}/search/v2/articlesearch.json?q=${queryString}&api-key=${NewsSources.nytimes.apiKey}`);
+    const { data } = await api.get(
+      `${NewsSources.nytimes.baseURL}/search/v2/articlesearch.json?q=${queryString}&api-key=${NewsSources.nytimes.apiKey}`
+    );
     return data;
   } catch (error) {
     return api.handleError(error);
   }
+};
 
-
-}
-
-
-
-export const responseSerialiser = ({newsapiResponse,theguardianResponse,nytimesResponse}) => {
-  console.log("newsapiResponse,theguardianResponse,nytimesResponse",newsapiResponse,theguardianResponse,nytimesResponse)
+export const responseSerialiser = ({
+  newsapiResponse,
+  theguardianResponse,
+  nytimesResponse,
+}) => {
+  console.log(
+    "newsapiResponse,theguardianResponse,nytimesResponse",
+    newsapiResponse,
+    theguardianResponse,
+    nytimesResponse
+  );
   let categoryList = [];
   let dateRange = [];
   let articleList = [];
 
-  return {categoryList,dateRange,articleList};
-
-}
+  return { categoryList, dateRange, articleList };
+};
